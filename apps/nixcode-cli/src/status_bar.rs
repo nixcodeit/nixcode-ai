55064e7a07c4eb759a2cc47752a7add7e8bff4e1
Damian Plewa <damianp96@gmail.com>
2025-03-30 03:02:26 +0200

    feat: added displaying current version

--- apps/nixcode-cli/src/status_bar.rs
+++ apps/nixcode-cli/src/status_bar.rs
@@ -22,12 +22,23 @@ impl Widget for StatusBar {
     where
         Self: Sized,
     {
+        // Get application version from Cargo.toml
+        const VERSION: &str = env!("CARGO_PKG_VERSION");
+
         let now = chrono::Local::now();
         let formatted_date = now.format("%d/%m/%Y %H:%M:%S").to_string();
-        let horizontal = Layout::horizontal([Fill(1), Length(formatted_date.len() as u16)]);
-        let [inner_area, date_area] = horizontal.areas(area.inner(Margin::new(1, 0)));
+        let version_text = format!("v{}", VERSION);
+
+        // Calculate total length of the right side content (date + version)
+        let right_content_length = formatted_date.len() + 1 + version_text.len();
+
+        // Create layout with three sections: mode info, fill space, date+version
+        let horizontal = Layout::horizontal([Fill(1), Length(right_content_length as u16)]);
+        let [inner_area, right_area] = horizontal.areas(area.inner(Margin::new(1, 0)));
 
         Block::new().bg(Color::DarkGray).render(area, buf);
+
+        // Render the mode info on the left
         Line::from(vec![
             Span::raw("Mode: "),
             Span::styled(
@@ -39,6 +50,18 @@ impl Widget for StatusBar {
             ),
         ])
         .render(inner_area, buf);
-        Span::from(formatted_date).render(date_area, buf);
+
+        // Render date and version on the right
+        Line::from(vec![
+            Span::styled(
+                version_text,
+                Style::new()
+                    .fg(Color::LightBlue)
+                    .add_modifier(Modifier::BOLD),
+            ),
+            Span::raw(" "),
+            Span::from(formatted_date),
+        ])
+        .render(right_area, buf);
     }
 }
